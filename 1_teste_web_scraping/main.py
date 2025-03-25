0d15c74ea3a4d739b5f1f73f9846daa0a3a8ebc8
Amir Albuquerque <amirsilva00an@gmail.com>
2025-03-25 10:47:37 -0300

    Implementação da extração de dados

--- 1_teste_web_scraping/main.py
+++ 1_teste_web_scraping/main.py
@@ -3,7 +3,8 @@ import requests, zipfile, os
 url_anexo1 = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos/Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf"
 url_anexo2 = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos/Anexo_II_DUT_2021_RN_465.2021_RN628.2025_RN629.2025.pdf"
 i = 1
-
+curr_dir = os.getcwd()
+project_dir = f"{curr_dir}/1_teste_web_scraping/"
 while i != 3:
     if i == 1:
         url = url_anexo1
@@ -14,7 +15,7 @@ while i != 3:
     response = requests.get(url)
     #Salvando PDF
     if response.status_code == 200:
-        with open(f"Anexo_{i}.pdf","wb") as file:
+        with open(f"{project_dir}Anexo_{i}.pdf","wb") as file:
             file.write(response.content)
         print("PDF salvo com sucesso!")
     else:
@@ -27,5 +28,5 @@ def compactar(caminho_do_zip, arquivos_para_compactar):
             zipf.write(arquivo, os.path.basename(arquivo))
     print(f"Arquivo compactados em {caminho_do_zip}")
 
-arquivos = ["Anexo_1.pdf", "Anexo_2.pdf"]
-compactar("Anexos.zip", arquivos)
+arquivos = [f"{project_dir}Anexo_1.pdf", f"{project_dir}Anexo_2.pdf"]
+compactar(f"{project_dir}Anexos.zip", arquivos)
--- 2_teste_transformacao_dados/main.py
+++ 2_teste_transformacao_dados/main.py
@@ -1,11 +1,47 @@
 import pandas as pd
-import pdfplumber, os
+import pdfplumber, os, zipfile
 
 file_name = "Anexo_1.pdf"
-relative_file_path = f"../1_teste_web_scraping/{file_name}"
+save_name = "/tabela_extraida.csv"
+relative_file_path = f"./1_teste_web_scraping/{file_name}"
+relative_save_path = f"./2_teste_transformacao_dados"
 abs_file_path = os.path.abspath(relative_file_path)
+abs_save_path = os.path.abspath(relative_save_path)
 
 if os.path.exists(abs_file_path):
     print(f"Arquivo {file_name} encontrado na pasta: {abs_file_path}")
+
+    with pdfplumber.open(abs_file_path) as pdf:
+        all_tables = []
+
+    # Iterar por todas as páginas do PDF
+        for page_num, page in enumerate(pdf.pages, start=1):
+            tables = page.extract_tables()
+            
+            # Se houver tabelas na página, processá-las
+            for table in tables:
+                df = pd.DataFrame(table)  # Converte para DataFrame do Pandas
+                df.columns = df.iloc[0]  # Define a primeira linha como cabeçalho
+                df = df[1:].reset_index(drop=True)  # Remove a linha duplicada do cabeçalho
+                all_tables.append(df)  # Adiciona ao conjunto de tabelas extraídas
+                
+                print(f"\rExtraíndo tabela da página {page_num}...",end="")
+        print(f"\nExtração completa!\n")
+
+    df_final = pd.concat(all_tables, ignore_index=True)
+    df_final.columns = [*df_final.columns[:3],"Seg. Odontológia","Seg. Ambulatórial",*df_final.columns[5:]]
+
+    df_final.to_csv(f"{abs_save_path+save_name}", index=False, encoding = "utf-8")
+    print(f"Tabela salva como 'tabela_extraida.csv' na pasta {abs_save_path}.")
 else:
     print(f"Arquivo {file_name} não encontrado na pasta {os.getcwd()}")
+
+
+def compactar(caminho_do_zip, arquivos_para_compactar):
+    with zipfile.ZipFile(caminho_do_zip,"w") as zipf:
+        for arquivo in arquivos_para_compactar:
+            zipf.write(arquivo, os.path.basename(arquivo))
+    print(f"Arquivo compactado na pasta {caminho_do_zip}")
+
+arquivos = [f"{abs_save_path+save_name}"]
+compactar(f"{abs_save_path}/Teste_{{Amir}}.zip", arquivos)
